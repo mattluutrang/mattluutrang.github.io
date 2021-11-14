cacecf1c19ff2916262ad54f90ea96c5354ecb0e
trangml <mattluutrang@vt.edu>
2021-11-14 12:43:19 -0500

    Fixed victory screen

--- games_src/Video-Game-Final-Project/screens/victory-screen.js
+++ games_src/Video-Game-Final-Project/screens/victory-screen.js
@@ -3,6 +3,7 @@
  **/
 
 let victoryWave;
+let victoryWaveHeight;
 let victoryBoids = [];
 let victoryCharacter;
 let startVictoryTime;
@@ -19,43 +20,12 @@ function initVictoryScreenVariables() {
     victoryOrc = new Orc(width / 2 - 6, 300);
     victoryOrc.stopFlag = true;
     victoryCharacter.direction = "none"
-    startGameAnimation = false;
     playButton = new Button(150, 200, 100, 40, "#E5CD6C", "Play", 20, "#f5712a", byteFont);
-    waveHeight = 7.2 * height / 8;
+    victoryWaveHeight = 7.2 * height / 8;
     victoryBoat = new Boat(40, 6.6 * height / 8, 40, true, 0.25);
-    noiseA = 700;
     startVictoryTime = frameCount;
-
 }
 
-function drawGameStartAnimation() {
-    if (waveHeight > -10) {
-        waveHeight -= height / 60;
-    }
-    else {
-        gameState = "startInstructions";
-    }
-}
-
-function drawClouds() {
-    // sky
-    noStroke();
-    var n1 = noiseA;
-    for (var x = 0; x <= 400; x += 4) {
-        var n2 = 0;
-        for (var y = 0; y <= 150; y += 4) {
-            var c = 255 * noise(n1, n2);
-            if (c > 150) {
-                fill(c + 30, c + 10, c, 200);
-                circle(x, y, 10)
-                //rect(x, y, 12, 12, 12);
-            }
-            n2 += 0.05; // step size in noise
-        }
-        n1 += 0.02; // step size in noise
-    }
-    noiseA -= 0.002;  // speed of clouds
-}
 function drawVictoryBackground() {
     // draw sky background (change to gradient image later)
     stroke('#ADD8E6');
@@ -91,7 +61,7 @@ function drawVictoryBackground() {
     // draw water
     fill("#639bff");
     push();
-    translate(0, waveHeight);
+    translate(0, victoryWaveHeight);
     victoryWave[0].update();
     victoryWave[0].draw();
     victoryWave[1].update();
@@ -140,16 +110,14 @@ function drawVictoryBoxes() {
 // TODO: Make this look a lot better
 function drawVictoryScreen() {
     drawVictoryBackground();
-    if (!startGameAnimation) {
-        drawVictoryBoxes();
-    }
+    drawVictoryBoxes();
 }
 
-function victoryScreenClickedLogic() {
-    if (playButton.isClicked()) {
-        if (!startGameAnimation) {
-            startGameAnimation = true;
-            startTime = frameCount;
-        }
-    }
-}
+// function victoryScreenClickedLogic() {
+//     if (playButton.isClicked()) {
+//         if (!startGameAnimation) {
+//             startGameAnimation = true;
+//             startTime = frameCount;
+//         }
+//     }
+// }
